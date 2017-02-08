3b15b40a3daa18fccb47fc1fbf909ff1e8adcc5d
LB <l.buerkner@gmail.com>
2017-02-08 19:14:50 +0100

    Updated "ProfileActivity.java" with an additopnal Method "SaveUserInformation" Created a new class "UserInformation"
    updated the "activity_profile.xml" and added 2 field to storage user data and a "Save" Button
    
    Tutorial Step 3 finished

--- app/src/main/java/development/android/androidfirebasetutorial/ProfileActivity.java
+++ app/src/main/java/development/android/androidfirebasetutorial/ProfileActivity.java
@@ -5,11 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -19,6 +22,14 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     //view objects
     private TextView textViewUserEmail;
     private Button buttonLogout;
+
+    //Database Objects
+    private DatabaseReference databaseReference;
+    private EditText editTextName, editTextAdress;
+    private Button buttonSave;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +50,13 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             startActivity(new Intent(this, LoginActivity.class));
         }
 
+        //Initialize FirebaseDatabase /* get the instance of the account that is logged in??? */
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        //Initialize the two data fields and the Save button
+        editTextAdress = (EditText) findViewById(R.id.editTextAddress);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        buttonSave = (Button) findViewById(R.id.buttonSave);
+
         /* If the user is loggen in proceed with this code*/
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
@@ -51,6 +69,34 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         //adding listener to button
         buttonLogout.setOnClickListener(this);
+        buttonSave.setOnClickListener(this);
+    }
+
+    /* Uses the Firebase Database to store data */
+    private void SaveUserInformation(){
+
+        String name = editTextName.getText().toString().trim();
+        String address = editTextAdress.getText().toString().trim();
+
+        //define object for the class UserInformation
+        UserInformation userInformation = new UserInformation(name, address);
+
+        //Get the unique ID of the logged in user to store data in the Firebase Database
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        //Set the databasereference to the unique Id of the current user and pass the Userinformation to the database
+        databaseReference.child(user.getUid()).setValue(userInformation);
+
+        //NEU NACH DEM TUTORIAL HINZUGEFÜGT
+        /* The TestEdit fields are being emptied after the database storage*/
+        editTextName.setText("");
+        editTextAdress.setText("");
+
+        //Show message that the information was saved
+        Toast.makeText(this, "Information Saved...", Toast.LENGTH_LONG).show();
+
+
+
     }
 
     @Override
@@ -65,6 +111,13 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             finish();
             //starting login activity
             startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        //if Save Information is pressed
+        if (view == buttonSave){
+            // call the method SaveUSerInformation
+            SaveUserInformation();
+
         }
 
     }
---
+++ app/src/main/java/development/android/androidfirebasetutorial/UserInformation.java
@@ -0,0 +1,21 @@
+package development.android.androidfirebasetutorial;
+
+/**
+ * Created by lbuer on 08.02.2017.
+ */
+
+public class UserInformation {
+
+    public String name;
+    public String address;
+
+    public UserInformation(){
+
+    }
+
+    //Generated constructor (done via rightclick)
+    public UserInformation(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
