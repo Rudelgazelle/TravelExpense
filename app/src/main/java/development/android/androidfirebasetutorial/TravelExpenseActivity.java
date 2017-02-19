8f199f934c36325c18acca66e60f172b3a6e20c2
LB <l.buerkner@gmail.com>
2017-02-19 20:36:24 +0100

    added fields for the travelexpense activity

--- app/src/main/java/development/android/androidfirebasetutorial/TravelExpenseActivity.java
+++ app/src/main/java/development/android/androidfirebasetutorial/TravelExpenseActivity.java
@@ -2,11 +2,17 @@ package development.android.androidfirebasetutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class TravelExpenseActivity extends AppCompatActivity {
 
     private EditText editTextDate;
+    private Spinner spinnerExpenseType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,19 @@ public class TravelExpenseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_travel_expense);
 
         editTextDate = (EditText) findViewById(R.id.editTextDate);
+
+        // Set the spinner Items
+        spinnerExpenseType = (Spinner)findViewById(R.id.spinnerExpenseType);
+        String[] items = new String[]{"Taxi", "Train", "Food"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        spinnerExpenseType.setAdapter(adapter);
+
+        //Set default Travel date as today
+        Calendar calendar = Calendar.getInstance();
+
+        SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+        String travelDate = df.format(calendar.getTime());
+        editTextDate.setText(travelDate);
 
     }
 }
