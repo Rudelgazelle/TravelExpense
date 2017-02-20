2c1cb04d232649e52026cc51d789965b22b7108b
LB <l.buerkner@gmail.com>
2017-02-20 21:58:00 +0100

    updated methods to TravelExpenseActivity

--- app/src/main/java/development/android/androidfirebasetutorial/TravelExpenseActivity.java
+++ app/src/main/java/development/android/androidfirebasetutorial/TravelExpenseActivity.java
@@ -6,26 +6,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class TravelExpenseActivity extends AppCompatActivity {
 
-    private EditText editTextDate;
+    private EditText editTextTravelDate;
     private Spinner spinnerExpenseType;
+    private EditText editTextExpenseAmount;
+    private EditText editTextTravelDescription;
+    private EditText editTextTravelLocation;
+
+    private String travelDate;
+
+    private Button btnSave;
+
+    private FirebaseAuth firebaseAuth;
+
+    //Database Objects
+    private DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travel_expense);
 
-        editTextDate = (EditText) findViewById(R.id.editTextDate);
+        editTextTravelDate = (EditText) findViewById(R.id.editTextDate);
+        editTextExpenseAmount = (EditText) findViewById(R.id.editTextExpenseAmount);
+        editTextTravelDescription = (EditText)findViewById(R.id.editTextDescr);
+        editTextTravelLocation = (EditText) findViewById(R.id.editTextLocation);
 
         // Set the spinner Items
         spinnerExpenseType = (Spinner)findViewById(R.id.spinnerExpenseType);
@@ -35,58 +56,46 @@ public class TravelExpenseActivity extends AppCompatActivity {
 
         //Set default Travel date as today
         Calendar calendar = Calendar.getInstance();
-
         SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy");
-        String travelDate = df.format(calendar.getTime());
-        editTextDate.setText(travelDate);
-
+        travelDate = df.format(calendar.getTime());
+        editTextTravelDate.setText(travelDate);
     }
-
-    // DIESE METHODEN MÜSSEN NOCH ANGEPASST WERDEN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     /* Uses the Firebase Database to store data */
     private void SaveTravelExpenseData(){
 
-        String name = editTextName.getText().toString().trim();
-        String address = editTextAdress.getText().toString().trim();
+        travelDate = editTextTravelDate.getText().toString().trim();
+        String expenseType = spinnerExpenseType.getSelectedItem().toString();
+        Float expenseAmount = Float.valueOf(editTextExpenseAmount.getText().toString().trim());
+        String description = editTextTravelDescription.getText().toString().trim();
+        String location = editTextTravelLocation.getText().toString().trim();
 
         //define object for the class UserInformation
-        UserInformation userInformation = new UserInformation(name, address);
+        TravelExpenseData travelExpenseData = new TravelExpenseData(travelDate, expenseType, expenseAmount, description, location);
 
         //Get the unique ID of the logged in user to store data in the Firebase Database
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
         //Set the databasereference to the unique Id of the current user and pass the Userinformation to the database
-        databaseReference.child(user.getUid()).setValue(userInformation);
+        databaseReference.child(user.getUid()).setValue(travelExpenseData);
 
         //NEU NACH DEM TUTORIAL HINZUGEFÜGT
         /* The TestEdit fields are being emptied after the database storage*/
-        editTextName.setText("");
-        editTextAdress.setText("");
+        editTextTravelDate.setText("");
+        editTextExpenseAmount.setText("");
+        editTextTravelDescription.setText("");
+        editTextTravelLocation.setText("");
 
         //Show message that the information was saved
         Toast.makeText(this, "Information Saved...", Toast.LENGTH_LONG).show();
-
     }
 
     public void onClick(View view) {
 
-        //if logout is pressed
-        if (view == buttonLogout){
-            //logging out the user
-            firebaseAuth.signOut();
-            Toast.makeText(ProfileActivity.this,"Logout successful",Toast.LENGTH_SHORT).show();
-            //closing activity
-            finish();
-            //starting login activity
-            startActivity(new Intent(this, LoginActivity.class));
-        }
-
         //if Save Information is pressed
-        if (view == buttonSave){
+        if (view == btnSave){
             // call the method SaveUSerInformation
-            SaveUserInformation();
-
+            SaveTravelExpenseData();
         }
 
     }
--- app/src/main/java/development/android/androidfirebasetutorial/TravelExpenseData.java
+++ app/src/main/java/development/android/androidfirebasetutorial/TravelExpenseData.java
@@ -1,21 +1,15 @@
 package development.android.androidfirebasetutorial;
 
-import java.util.Date;
-
-/**
- * Created by lbuer on 19.02.2017.
- */
-
 public class TravelExpenseData {
 
-    public Date travelDate;
+    public String travelDate;
     public String expenseType;
     public Float expenseAmount;
     public String location;
     public String description;
 
 
-    public TravelExpenseData( Date travelDate, String expenseType, Float expenseAmount, String location, String description) {
+    public TravelExpenseData( String travelDate, String expenseType, Float expenseAmount, String location, String description) {
 
         this.travelDate = travelDate;
         this.expenseType = expenseType;
